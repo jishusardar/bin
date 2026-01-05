c9ffa68e1142ca9418e713cfe37546372eca566e
Jishu Sardar <jishusardar01@gmail.com>
2026-01-05 22:55:47 +0530

    javapattern

---
+++ Java/Basic/Basicprob.java
@@ -0,0 +1,393 @@
+package Basic;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+class Basicprob {
+  public static void main(String[]args)
+  {
+      //pattern 1
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The Nth Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              System.out.print("*");
+//              System.out.print(" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern 2
+//      int n,m;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N term: ");
+//      n=s.nextInt();
+//      System.out.print("Entre The M Term: ");
+//      m=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              System.out.print(m+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern3
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              System.out.print(i+1+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern 4
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              System.out.print(j+1+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern 5
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              System.out.print(n-j+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern 6
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              System.out.print((j+1)*(j+1)+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern 7
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              char b=(char) ('a'+j);
+//              System.out.print(b+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern 8
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              char b=(char) ('a'+i);
+//              System.out.print(b+" ");
+//          }
+//          System.out.print("\n");
+//      }
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      int sum=0;
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n;j++){
+//              System.out.print(++sum+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 1
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<=i;j++){
+//              System.out.print("*"+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 2
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<=i;j++){
+//              System.out.print(j+1+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 3
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<=i;j++){
+//              System.out.print(i+1+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 4
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<=i;j++){
+//              System.out.print((i+1)-j+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 5
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<=i;j++){
+//              char c=(char)('a'+i);
+//              System.out.print(c+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 6
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i;j++){
+//              System.out.print("*"+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 7
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i;j++){
+//              System.out.print(j+1+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 8
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<=i;j++){
+//              System.out.print(n-j+" ");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 9
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i-1;j++){
+//              System.out.print(" ");
+//          }
+//          for(int j=0;j<=i;j++){
+//              System.out.print("*");
+//          }
+//          System.out.print("\n");
+//      }
+//      //pattern printing 10
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The Nth Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i-1;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<=i;j++)
+//              System.out.print(i+1);
+//          System.out.print("\n");
+//      }
+      //pattern printing 11
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The Nth Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i-1;j++)
+//              System.out.print(" ");
+//          for (int j=0;j<=i;j++)
+//              System.out.print(j+1);
+//          System.out.print("\n");
+//      }
+      //pattern printing 12
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The Nth Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i-1;j++){
+//              System.out.print(" ");
+//          }
+//          for(int j=0;j<=i;j++){
+//              char c=(char)('A'+j);
+//              System.out.print(c);
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 13
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N th Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i-1;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<=i;j++){
+//              System.out.print(i+1-j);
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 14
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N Th Term: ");
+//      n=s.nextInt();
+//      for (int i=0;i<n;i++){
+//          for(int j=0;j<n-i-1;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<=i;j++){
+//              System.out.print("*");
+//          }
+//          for(int j=1;j<=i;j++){
+//              System.out.print("*");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 15
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The N th Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i-1;j++){
+//              System.out.print(" ");
+//          }
+//          for(int j=0;j<=i;j++)
+//              System.out.print(j+1);
+//          for(int j=0;j<i;j++)
+//              System.out.print(i-j);
+//          System.out.print("\n");
+//      }
+      //pattern printing 16
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The n th Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<i;j++){
+//              System.out.print(" ");
+//          }
+//          for(int j=0;j<n-i;j++){
+//              System.out.print("*");
+//          }
+//          for(int j=0;j<n-i-1;j++){
+//              System.out.print("*");
+//          }
+//          System.out.print("\n");
+//      }
+      //pattern printing 17
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entre The n th Term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<n-i;j++)
+//              System.out.print("*");
+//          for(int j=0;j<i;j++)
+//              System.out.print(" ");
+//          for (int j=0;j<i;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<n-i;j++){
+//              System.out.print("*");
+//          }
+//          System.out.print("\n");
+//      }
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<=i;j++){
+//              System.out.print("*");
+//          }
+//          for(int j=0;j<n-i-1;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<n-i-1;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<=i;j++)
+//              System.out.print("*");
+//          System.out.print("\n");
+//      }
+      //pattern printing 18
+//      int n;
+//      Scanner s=new Scanner(System.in);
+//      System.out.print("Entrte The nth term: ");
+//      n=s.nextInt();
+//      for(int i=0;i<n;i++){
+//          for(int j=0;j<=i;j++)
+//              System.out.print("*");
+//          for(int j=0;j<n-i-1;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<n-i-1;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<=i;j++)
+//              System.out.print("*");
+//          System.out.print("\n");
+//      }
+//      for(int i=1;i<n;i++){
+//          for(int j=0;j<n-i;j++)
+//              System.out.print("*");
+//          for(int j=0;j<i;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<i;j++)
+//              System.out.print(" ");
+//          for(int j=0;j<n-i;j++)
+//              System.out.print("*");
+//          System.out.print("\n");
+//      }
+      //pattern printing 19
+      int n;
+      Scanner s=new Scanner(System.in);
+      System.out.print("Entre The n term: ");
+      n=s.nextInt();
+      for(int i=0;i<n;i++){
+          for(int j=0;j<n-i-1;j++){
+              System.out.print(" ");
+          }
+          for(int j=0;j<=i;j++)
+              System.out.print("*"+" ");
+          System.out.print("\n");
+      }
+      for(int i=0;i<n;i++){
+          for(int j=0;j<i;j++)
+              System.out.print(" ");
+          for(int j=0;j<n-i;j++)
+              System.out.print("*"+" ");
+          System.out.print("\n");
+      }
+  }
+}
