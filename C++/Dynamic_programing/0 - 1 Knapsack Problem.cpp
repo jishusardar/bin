af600baaacb584b8a082950de86fad03ac2ebeba
Jishu Sardar <jishusardar01@gmail.com>
2026-02-15 20:48:10 +0530

    dp coinchange

---
+++ C++/Dynamic_programing/0 - 1 Knapsack Problem.cpp
@@ -0,0 +1,104 @@
+//
+// Created by jishu on 15-02-2026.
+//0-1 knapsack problem
+//brute force using recursion
+// #include<iostream>
+// #include <vector>
+// using namespace std;
+// int find(int index,int w,vector<int>&val,vector<int>&wt) {
+//     if (w==0)
+//         return 0;
+//     if (index==0)
+//         return 0;
+//     if (wt[index-1]>w)
+//         return find(index-1,w,val,wt);
+//     else
+//         return max(val[index-1]+find(index-1,w-wt[index-1],val,wt),find(index-1,w,val,wt));
+// }
+// int knapsack_problem(int W,vector<int>&val,vector<int>&wt) {
+//     int n=wt.size();
+//     return find(n,W,val,wt);
+// }
+// int main() {
+//     int n,w;
+//     cout<<"Entre The Size: ";
+//     cin>>n;
+//     cout<<"Entre The Weight: ";
+//     cin>>w;
+//     vector<int>val(n);
+//     vector<int>wt(n);
+//     cout<<"Entre The Weights: ";
+//     for (int i=0;i<n;i++)
+//         cin>>wt[i];
+//     cout<<"Entre The Values: ";
+//     for (int i=0;i<n;i++)
+//         cin>>val[i];
+//     cout<<knapsack_problem(w,val,wt);
+// }
+//top down approach
+// #include<iostream>
+// #include <vector>
+// using namespace std;
+// int find(int index,int w,vector<int>&val,vector<int>&wt,vector<vector<int>>&dp) {
+//     if (w==0)
+//         return 0;
+//     if (index==0)
+//         return 0;
+//     if (dp[index][w]!=-1)
+//         return dp[index][w];
+//     if (wt[index-1]>w)
+//         return dp[index][w]=find(index-1,w,val,wt,dp);
+//     else
+//         return dp[index][w]=max(val[index-1]+find(index-1,w-wt[index-1],val,wt,dp),find(index-1,w,val,wt,dp));
+// }
+// int knapsack_problem(int w,vector<int>&val,vector<int>&wt) {
+//     int n=wt.size();
+//     vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
+//     return find(n,w,val,wt,dp);
+// }
+// int main() {
+//     int n,w;
+//     cout<<"Entre The Size: ";
+//     cin>>n;
+//     cout<<"Entre The Weight: ";
+//     cin>>w;
+//     vector<int>val(n);
+//     cout<<"Entre The values: ";
+//     for (int i=0;i<n;i++)
+//         cin>>val[i];
+//     vector<int>wt(n);
+//     cout<<"Entre The Weights: ";
+//     for (int i=0;i<n;i++)
+//         cin>>wt[i];
+//     cout<<knapsack_problem(w,val,wt);
+// }
+//bottom up apparoach
+// #include<iostream>
+// #include <vector>
+// using namespace std;
+// int main() {
+//     int n,w;
+//     cout<<"Entre The Size: ";
+//     cin>>n;
+//     cout<<"Entre The Weight: ";
+//     cin>>w;
+//     vector<int>val(n);
+//     cout<<"Entre The values: ";
+//     for (int i=0;i<n;i++)
+//         cin>>val[i];
+//     vector<int>wt(n);
+//     cout<<"Entre The Weights: ";
+//     for (int i=0;i<n;i++)
+//         cin>>wt[i];
+//     vector<vector<int>>dp(n+1,vector<int>(w+1,0));
+//     for (int i=1;i<=n;i++)
+//         for (int j=1;j<=w;j++) {
+//             if (wt[i-1]>j)
+//                 dp[i][j]=dp[i-1][j];
+//             else
+//                 dp[i][j]=max(val[i-1]+ dp[i-1][j-wt[i-1]],dp[i-1][j]);
+//         }
+//
+//     cout<<dp[n][w];
+// }
+//more optimized
--- C++/Dynamic_programing/coins_change.cpp
+++ C++/Dynamic_programing/coins_change.cpp
@@ -1,36 +1,35 @@
 //
 // Created by jishu on 11-02-2026.
-//coins change in backward approach bruteforce
-#include<iostream>
-#include <vector>
-using namespace std;
-int find(int n,int amount,vector<int>&coins) {
-    if (amount==0)
-        return 1;
-    if (n<0)
-        return 0;
-    if (coins[n]>amount)
-        return find(n-1,amount,coins);
-    else
-        return find(n,amount-coins[n],coins)+find(n-1,amount,coins);
-}
-int coin_change(int amount,vector<int>&coins) {
-    int n=coins.size();
-    return find(n-1,amount,coins);
-}
-int main() {
-    int n;
-    cout<<"Entre The Size: ";
-    cin>>n;
-    vector<int>arr(n);
-    for (int i=0;i<n;i++)
-        cin>>arr[i];
-    int target;
-    cout<<"Entre The Target: ";
-    cin>>target;
-    cout<<coin_change(target,arr);
-
-}
+// //coins change in backward approach bruteforce
+// #include<iostream>
+// #include <vector>
+// using namespace std;
+// int find(int n,int amount,vector<int>&coins) {
+//     if (amount==0)
+//         return 1;
+//     if (n<0)
+//         return 0;
+//     if (coins[n]>amount)
+//         return find(n-1,amount,coins);
+//     else
+//         return find(n,amount-coins[n],coins)+find(n-1,amount,coins);
+// }
+// int coin_change(int amount,vector<int>&coins) {
+//     int n=coins.size();
+//     return find(n-1,amount,coins);
+// }
+// int main() {
+//     int n;
+//     cout<<"Entre The Size: ";
+//     cin>>n;
+//     vector<int>arr(n);
+//     for (int i=0;i<n;i++)
+//         cin>>arr[i];
+//     int target;
+//     cout<<"Entre The Target: ";
+//     cin>>target;
+//     cout<<coin_change(target,arr);
+// }
 //coins change using top down approach in backward
 // #include<iostream>
 // #include <vector>
@@ -64,3 +63,61 @@ int main() {
 //     cin>>target;
 //     cout<<coins_change(target,arr);
 // }
+//bottom up approach
+// #include<iostream>
+// #include <vector>
+// using namespace std;
+// int coin_change(int amount,vector<int>&coins) {
+//     int n=coins.size();
+//     vector<vector<int>>dp(n+1,vector<int>(amount+1,0));
+//     for (int i=0;i<=n;i++)
+//         dp[i][0]=1;
+//     for (int i=1;i<=n;i++)
+//         for (int j=1;j<=amount;j++) {
+//             if (coins[i-1]>j)
+//                 dp[i][j]=dp[i-1][j];
+//             else
+//                 dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+//         }
+//     return dp[n][amount];
+// }
+// int main() {
+//     int n,amount;
+//     cout<<"Entre the Size: ";
+//     cin>>n;
+//     vector<int>coins(n);
+//     cout<<"Entre The Element: ";
+//     for (int i=0;i<n;i++)
+//         cin>>coins[i];
+//     cout<<"Entre The amount: ";
+//     cin>>amount;
+//     cout<<coin_change(amount,coins);
+// }
+//more optimized approach
+#include<iostream>
+#include <vector>
+using namespace std;
+int coin_change(int amount,vector<int>&coins) {
+    vector<int>dp(amount+1,0);
+    dp[0]=1;
+    int n=coins.size();
+    for (int i=1;i<=n;i++)
+        for (int j=1;j<=amount;j++) {
+            if (coins[i-1]<=j)
+                dp[j]+=dp[j-coins[i-1]];
+        }
+    return dp[amount];
+}
+int main() {
+    int n;
+    cout<<"Entre The Size: ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"Entre The coins: ";
+    for (int i=0;i<n;i++)
+        cin>>arr[i];
+    int amount;
+    cout<<"Entre The Amount: ";
+    cin>>amount;
+    cout<<coin_change(amount,arr);
+}
